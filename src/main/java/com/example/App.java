8d96d712f7b6e44c8ae8d550539a9e3b95f2f29b
Nayan16072001 <2021wa86688@wilp.bits-pilani.ac.in>
2025-06-15 17:17:35 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,13 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
  
-@SpringBootTest
-class CodeInspectionApplicationTests {
+@SpringBootApplication
+public class CodeInspectionApplication {
  
-	@Test
-	void contextLoads() {
-	}
- 
-}
+    public static void main(String[] args) {
+        SpringApplication.run(CodeInspectionApplication.class, args);
+    }
+
+} 
